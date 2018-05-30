c1ebd63c7dff4d7b08cf67f5a97ae9b76bef1e98
Luk Burchard <lbb@google.com>
2018-05-30 16:06:45 -0700

    Add Index structure
    Change-Id: I6fbb91138f52156e8ea88ce28966cb2232bf9c09

---
+++ pkg/index/types.go
@@ -0,0 +1,58 @@
+//  Copyright © 2018 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package index
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// Index is a top-level type.
+// TODO(lbb): Add deepcopy code generation.
+type Index struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec IndexSpec `json:"spec"`
+}
+
+type IndexSpec struct {
+	Version     string `json:"version,omitempty"`
+	Intro       string `json:"intro,omitempty"`
+	Description string `json:"description,omitempty"`
+	Caveats     string `json:"caveats,omitempty"`
+
+	Platforms []Platform `json:"platforms,omitempty"`
+}
+
+type Platform struct {
+	Head   string `json:"head,omitempty"`
+	URI    string `json:"uri,omitempty"`
+	Sha256 string `json:"sha256,omitempty"`
+
+	Selector metav1.LabelSelector `json:"selector,omitempty"`
+	Files    []FileOperation      `json:"files"`
+}
+
+type FileOperation struct {
+	From string `json:"from,omitempty"`
+	To   string `json:"to,omitempty"`
+}
+
+type IndexList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+
+	Items []Index `json:"items"`
+}
