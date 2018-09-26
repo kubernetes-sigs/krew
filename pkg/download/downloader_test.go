fa9866f549b86a1dbf87786f035e1c076eb7ea1b
Ahmet Alp Balkan <ahmetalpbalkan@gmail.com>
2018-09-25 19:58:32 -0700

    Add .tar.gz support for archive downloads (#51)
    Added an extractTARGZ method + tests. Inferring archive types from url suffix.
    
    Fixes #34.

--- pkg/download/downloader_test.go
+++ pkg/download/downloader_test.go
@@ -18,6 +18,8 @@ import (
 	"io/ioutil"
 	"os"
 	"path/filepath"
+	"reflect"
+	"strings"
 	"testing"
 )
 
@@ -29,18 +31,25 @@ func testdataPath() string {
 	return filepath.Join(pwd, "testdata")
 }
 
-func Test_extract(t *testing.T) {
+func Test_extractZIP(t *testing.T) {
 	tests := []struct {
-		filename, firstElementName string
-		isDir                      bool
+		in    string
+		files []string
 	}{
-		{"test.zip", "test", true},
-		{"test1.zip", "foo", false}, // No directory structure
+		{
+			in: "test-with-directory.zip",
+			files: []string{
+				"/test/",
+				"/test/foo"}},
+		{
+			in: "test-without-directory.zip",
+			files: []string{
+				"/foo"}},
 	}
 
 	for _, tt := range tests {
 		// Zip has just one file named 'foo'
-		zipSrc := filepath.Join(testdataPath(), tt.filename)
+		zipSrc := filepath.Join(testdataPath(), tt.in)
 		zipDst, err := ioutil.TempDir("", "")
 		if err != nil {
 			t.Fatal(err)
@@ -51,25 +60,77 @@ func Test_extract(t *testing.T) {
 		if err != nil {
 			t.Fatal(err)
 		}
+		defer zipReader.Close()
 		stat, _ := zipReader.Stat()
 		if err := extractZIP(zipDst, zipReader, stat.Size()); err != nil {
-			t.Fatalf("extract(%s) error = %v", tt.filename, err)
+			t.Fatalf("extractZIP(%s) error = %v", tt.in, err)
 		}
-		zipContent, err := ioutil.ReadDir(zipDst)
+
+		outFiles := collectFiles(t, zipDst)
+		if !reflect.DeepEqual(outFiles, tt.files) {
+			t.Fatalf("extractZIP(%s), expected=%#v, got=%#v", tt.in, tt.files, outFiles)
+		}
+	}
+}
+
+func Test_extractTARGZ(t *testing.T) {
+	tests := []struct {
+		in    string
+		files []string
+	}{
+		{
+			in: "test-with-directory.tar.gz",
+			files: []string{
+				"/test/",
+				"/test/foo"},
+		},
+		{
+			in:    "test-without-directory.tar.gz",
+			files: []string{"/foo"},
+		},
+	}
+
+	for _, tt := range tests {
+		tarSrc := filepath.Join(testdataPath(), tt.in)
+		tarDst, err := ioutil.TempDir("", "")
 		if err != nil {
 			t.Fatal(err)
 		}
+		defer os.RemoveAll(tarDst)
 
-		if len(zipContent) != 1 {
-			t.Fatalf("zip should just have one file got %d", len(zipContent))
+		tf, err := os.Open(tarSrc)
+		if err != nil {
+			t.Fatalf("failed to open %q. error=%v", tt.in, err)
 		}
-		for _, f := range zipContent {
-			if f.IsDir() != tt.isDir {
-				t.Fatalf("entity %q is should be isDir=%v but is %v", f.Name(), tt.isDir, f.IsDir())
-			}
-			if f.Name() != tt.firstElementName {
-				t.Fatalf("expected to find file %s, found %q", tt.firstElementName, f.Name())
-			}
+		defer tf.Close()
+
+		if err := extractTARGZ(tarDst, tf); err != nil {
+			t.Fatalf("failed to extract %q. error=%v", tt.in, err)
+		}
+
+		outFiles := collectFiles(t, tarDst)
+		if !reflect.DeepEqual(outFiles, tt.files) {
+			t.Fatalf("for %q, expected=%#v, got=%#v", tt.in, tt.files, outFiles)
 		}
 	}
+}
+
+// collectFiles lists the files by walking the path. It prefixes elements with
+// "/" and appends "/" to directories.
+func collectFiles(t *testing.T, scanPath string) []string {
+	var outFiles []string
+	if err := filepath.Walk(scanPath, func(fp string, info os.FileInfo, err error) error {
+		if fp == scanPath {
+			return nil
+		}
+		fp = strings.TrimPrefix(fp, scanPath)
+		if info.IsDir() {
+			fp = fp + "/"
+		}
+		outFiles = append(outFiles, fp)
+		return nil
+	}); err != nil {
+		t.Fatalf("failed to scan extracted dir %v. error=%v", scanPath, err)
+	}
+	return outFiles
 }
