96de068e5a13b4e10883682cdd9bc15f75716208
Cornelius Weig <cornelius.weig@gmail.com>
2019-06-04 22:15:12 +0200

    Fail fast in run-in-docker.sh

--- hack/run-in-docker.sh
+++ hack/run-in-docker.sh
@@ -14,6 +14,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+set -euo pipefail
+
 SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 docker build -f "${SCRIPTDIR}/sandboxed.Dockerfile" -t krew:sandbox "${SCRIPTDIR}/.."
