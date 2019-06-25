14f2c19902fab74bc473a58f64c3c5553a520654
Cornelius Weig <cornelius.weig@gmail.com>
2019-06-25 21:18:53 +0200

    Split long command line

--- hack/verify-code-patterns.sh
+++ hack/verify-code-patterns.sh
@@ -23,7 +23,11 @@ if [[ -n "$out" ]]; then
   exit 1
 fi
 
-out="$(grep --include '*.go' --exclude "*_test.go" --exclude 'constants.go' --exclude-dir 'vendor/' -EIrn '\.yaml"' || true)"
+out="$(grep --include '*.go' \
+            --exclude "*_test.go" \
+            --exclude 'constants.go' \
+            --exclude-dir 'vendor/' \
+            -EIrn '\.yaml"' || true)"
 if [[ -n "$out" ]]; then
   echo >&2 'You used ".yaml" in production, use constants.ManifestExtension instead:'
   echo >&2 "$out"
