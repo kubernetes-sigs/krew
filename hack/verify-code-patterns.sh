1305e199dfd3e741df1c72861bb9c4bad9f7c62e
JJJJJones <kaeyanojones@gmail.com>
2019-10-23 12:34:53 -0500

    support for testing from MacOS

--- hack/verify-code-patterns.sh
+++ hack/verify-code-patterns.sh
@@ -16,8 +16,20 @@
 
 set -euo pipefail
 
+# Appropriately set grep
+if [[ $(uname) == "Darwin" ]]; then
+  out="$(command -v ggrep)"
+  if [[ -n $out ]]; then
+    grep="ggrep"
+  else
+    echo >&2 "GNU grep is required for MacOS users to successfully run local tests; it can be found on homebrew as 'grep'"
+  fi
+else
+  grep="grep"
+fi
+
 # Disallow usage of ioutil.TempDir in tests in favor of testutil.
-out="$(grep --include '*_test.go' --exclude-dir 'vendor/' -EIrn 'ioutil.\TempDir' || true)"
+out="$(${grep} --include '*_test.go' --exclude-dir 'vendor/' -EIrn 'ioutil.\TempDir' || true)"
 if [[ -n "$out" ]]; then
   echo >&2 "You used ioutil.TempDir in tests, use 'testutil.NewTempDir()' instead:"
   echo >&2 "$out"
@@ -25,7 +37,7 @@ if [[ -n "$out" ]]; then
 fi
 
 # use code constant for ".yaml"
-out="$(grep --include '*.go' \
+out="$(${grep} --include '*.go' \
   --exclude "*_test.go" \
   --exclude 'constants.go' \
   --exclude-dir 'vendor/' \
@@ -37,7 +49,7 @@ if [[ -n "$out" ]]; then
 fi
 
 # Do not use glog in test code
-out="$(grep --include '*_test.go' --exclude-dir 'vendor/' -EIrn '[kg]log\.' || true)"
+out="$(${grep} --include '*_test.go' --exclude-dir 'vendor/' -EIrn '[kg]log\.' || true)"
 if [[ -n "$out" ]]; then
   echo >&2 "You used glog in tests, use 't.Logf' instead:"
   echo >&2 "$out"
@@ -45,7 +57,7 @@ if [[ -n "$out" ]]; then
 fi
 
 # Do not initialize index.{Plugin,Platform} structs in test code.
-out="$(grep --include '*_test.go' --exclude-dir 'vendor/' -EIrn '[^]](index\.)(Plugin|Platform){' || true)"
+out="$(${grep} --include '*_test.go' --exclude-dir 'vendor/' -EIrn '[^]](index\.)(Plugin|Platform){' || true)"
 if [[ -n "$out" ]]; then
   echo >&2 "Do not use index.Platform or index.Plugin structs directly in tests,"
   echo >&2 "use testutil.NewPlugin() or testutil.NewPlatform() instead:"
