d27e4618441fb4db7ace1d481cee3519dbdcccec
JJJJJones <kaeyanojones@gmail.com>
2019-11-05 11:44:27 -0600

    implemented structured output formatting (json and yaml) using genericclioptions; rewritten history properly

--- cmd/krew/cmd/list.go
+++ cmd/krew/cmd/list.go
@@ -24,14 +24,92 @@ import (
 
 	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/cli-runtime/pkg/genericclioptions"
+	"k8s.io/cli-runtime/pkg/printers"
 
 	"sigs.k8s.io/krew/pkg/installation"
 )
 
+type Entry struct {
+	Name    string
+	Version string
+}
+
+func sortByName(e []Entry) []Entry {
+	sort.Slice(e, func(a, b int) bool {
+		return e[a].Name < e[b].Name
+	})
+	return e
+}
+
+// Consume produces a junk GroupVersionKin for obj.GetObjectKind().GroupVersionKind().Empty() check to eat in PrintObj()
+type Consume struct {
+	APIVersion string
+	Kind       string
+}
+
+func (c Consume) SetGroupVersionKind(kind schema.GroupVersionKind) {
+	c.APIVersion, c.Kind = kind.ToAPIVersionAndKind()
+}
+
+func (c Consume) GroupVersionKind() schema.GroupVersionKind {
+	return schema.FromAPIVersionAndKind(c.APIVersion, c.Kind)
+}
+
+func (e Entry) GetObjectKind() schema.ObjectKind {
+	return Consume{"_", "_"}
+}
+
+func (e Entry) DeepCopyObject() runtime.Object {
+	newObj := Entry{e.Name, e.Version}
+	return newObj
+}
+
+type ListFlags struct {
+	JSONYamlPrintFlags *genericclioptions.JSONYamlPrintFlags
+	OutputFormat       *string
+}
+
+func NewListFlags() *ListFlags {
+	outputFormat := ""
+	return &ListFlags{
+		JSONYamlPrintFlags: genericclioptions.NewJSONYamlPrintFlags(),
+		OutputFormat:       &outputFormat,
+	}
+}
+
+func (f *ListFlags) AllowedFormats() []string {
+	formats := f.JSONYamlPrintFlags.AllowedFormats()
+	return formats
+}
+
+func (f *ListFlags) AddFlags(c *cobra.Command) {}
+
+func (f *ListFlags) ToPrinter() (printers.ResourcePrinter, error) {
+	outputFormat := *f.OutputFormat
+	var printer printers.ResourcePrinter
+
+	switch outputFormat {
+	case "json":
+		printer = &printers.JSONPrinter{}
+	case "yaml":
+		printer = &printers.YAMLPrinter{}
+	default:
+		return nil, genericclioptions.NoCompatiblePrinterError{OutputFormat: f.OutputFormat, AllowedFormats: f.AllowedFormats()}
+	}
+
+	return printer, nil
+}
+
 func init() {
+	outputFormat := ""
+	output := NewListFlags()
+
 	// listCmd represents the list command
 	listCmd := &cobra.Command{
-		Use:   "list",
+		Use:   "list [(-o|--output=)json|yaml|wide]",
 		Short: "List installed kubectl plugins",
 		Long: `Show a list of installed kubectl plugins and their versions.
 
@@ -46,7 +124,7 @@ Remarks:
 			}
 
 			// return sorted list of plugin names when piped to other commands or file
-			if !isTerminal(os.Stdout) {
+			if *output.OutputFormat == "" && !isTerminal(os.Stdout) {
 				var names []string
 				for name := range plugins {
 					names = append(names, name)
@@ -56,18 +134,53 @@ Remarks:
 				return nil
 			}
 
-			// print table
-			var rows [][]string
-			for p, version := range plugins {
-				rows = append(rows, []string{p, version})
+			if *output.OutputFormat == "wide" {
+				// print table
+				var rows [][]string
+				for p, version := range plugins {
+					rows = append(rows, []string{p, version})
+				}
+				rows = sortByFirstColumn(rows)
+				return printTable(os.Stdout, []string{"PLUGIN", "VERSION"}, rows)
 			}
-			rows = sortByFirstColumn(rows)
-			return printTable(os.Stdout, []string{"PLUGIN", "VERSION"}, rows)
+
+			if inArray(*output.OutputFormat, output.AllowedFormats()) {
+				objs := []Entry{}
+				for plugin, version := range plugins {
+					obj := Entry{plugin, version}
+					objs = append(objs, obj)
+				}
+				objs = sortByName(objs)
+				for _, obj := range objs {
+					p, err := output.ToPrinter()
+					if err != nil {
+						return err
+					}
+					err = p.PrintObj(obj, os.Stdout)
+					if err != nil {
+						return err
+					}
+				}
+				return nil
+			}
+			return errors.New("unsupported output format specified")
 		},
 		PreRunE: checkIndex,
 	}
 
+	listCmd.Flags().StringVarP(&outputFormat, "output", "o", outputFormat, "_")
+
+	output.OutputFormat = &outputFormat
 	rootCmd.AddCommand(listCmd)
+}
+
+func inArray(s string, arr []string) bool {
+	for _, a := range arr {
+		if s == a {
+			return true
+		}
+	}
+	return false
 }
 
 func printTable(out io.Writer, columns []string, rows [][]string) error {
--- go.mod
+++ go.mod
@@ -3,23 +3,37 @@ module sigs.k8s.io/krew
 go 1.12
 
 require (
+	github.com/emicklei/go-restful v2.11.1+incompatible // indirect
+	github.com/evanphx/json-patch v4.5.0+incompatible // indirect
 	github.com/fatih/color v1.7.0
-	github.com/gogo/protobuf v1.2.1 // indirect
+	github.com/go-openapi/jsonreference v0.19.3 // indirect
+	github.com/go-openapi/spec v0.19.4 // indirect
+	github.com/gogo/protobuf v1.3.1 // indirect
 	github.com/golang/glog v0.0.0-20160126235308-23def4e6c14b
-	github.com/google/go-cmp v0.3.0
-	github.com/inconshreveable/mousetrap v1.0.0 // indirect
+	github.com/google/go-cmp v0.3.1
+	github.com/googleapis/gnostic v0.3.1 // indirect
+	github.com/gregjones/httpcache v0.0.0-20190611155906-901d90724c79 // indirect
+	github.com/imdario/mergo v0.3.8 // indirect
+	github.com/json-iterator/go v1.1.8 // indirect
 	github.com/kylelemons/godebug v1.1.0 // indirect
-	github.com/mattn/go-colorable v0.1.2 // indirect
-	github.com/mattn/go-isatty v0.0.8
-	github.com/pkg/errors v0.8.0
-	github.com/sahilm/fuzzy v0.0.5
-	github.com/spf13/cobra v0.0.3
-	github.com/spf13/pflag v1.0.3
-	golang.org/x/net v0.0.0-20190628185345-da137c7871d7 // indirect
-	golang.org/x/sys v0.0.0-20190712062909-fae7ac547cb7 // indirect
+	github.com/mailru/easyjson v0.7.0 // indirect
+	github.com/mattn/go-colorable v0.1.4 // indirect
+	github.com/mattn/go-isatty v0.0.10
+	github.com/onsi/ginkgo v1.10.2 // indirect
+	github.com/pkg/errors v0.8.1
+	github.com/sahilm/fuzzy v0.1.0
+	github.com/spf13/cobra v0.0.5
+	github.com/spf13/pflag v1.0.5
+	github.com/stretchr/testify v1.4.0 // indirect
+	golang.org/x/crypto v0.0.0-20191029031824-8986dd9e96cf // indirect
+	golang.org/x/net v0.0.0-20191028085509-fe3aa8a45271 // indirect
+	golang.org/x/sys v0.0.0-20191029155521-f43be2a4598c // indirect
+	golang.org/x/time v0.0.0-20191024005414-555d28b269f0 // indirect
+	google.golang.org/appengine v1.6.5 // indirect
+	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
 	gopkg.in/inf.v0 v0.9.1 // indirect
-	k8s.io/apimachinery v0.0.0-20190717022731-0bb8574e0887
-	k8s.io/client-go v7.0.0+incompatible
-	k8s.io/klog v0.3.3 // indirect
+	k8s.io/apimachinery v0.0.0-20191102025618-50aa20a7b23f
+	k8s.io/cli-runtime v0.0.0-20191102031428-d1199d98239f
+	k8s.io/client-go v0.0.0-20191101230044-e9766ae82012
 	sigs.k8s.io/yaml v1.1.0
 )
