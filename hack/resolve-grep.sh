aef0973f58cbcf0df44f24b4f2f58eb06c337210
JJJJJones <kaeyanojones@gmail.com>
2020-01-29 17:24:02 -0600

    reference brew.sh

--- hack/resolve-grep.sh
+++ hack/resolve-grep.sh
@@ -3,7 +3,7 @@ function resolvegrep () {
     if [[ $(whereis ggrep) -eq 0 ]]; then
       ggrep $@
     else
-      echo "GNU grep not found!"
+      echo "GNU grep not found - please install GNU coreutils from brew.sh (brew install coreutils)"
       exit 1
     fi
   else
--- hack/resolve-link.sh
+++ hack/resolve-link.sh
@@ -3,7 +3,7 @@ function resolvelink () {
     if [[ $(whereis greadlink) -eq 0 ]]; then
       greadlink -f $@
     else
-      echo "GNU readlink not found"
+      echo "GNU readlink not found - please install GNU coreutils from brew.sh (brew install coreutils)"
       exit 1
     fi
   else
