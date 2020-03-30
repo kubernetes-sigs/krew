532d7fae2af441a30d2ced6744bceee6df50fe22
Ahmet Alp Balkan <ahmetb@google.com>
2020-03-30 14:30:42 -0700

    various naming utilities for canonicalization and display (#570)
    * various naming utilities for canonicalization and display
    
    Refactoring some of the utilities from the patch that ported 'krew list'
    to multi-indexes.
    
    These utilities will help us implement 'krew search'.
    
    Signed-off-by: Ahmet Alp Balkan <ahmetb@google.com>
    
    * rename file
    
    Signed-off-by: Ahmet Alp Balkan <ahmetb@google.com>

---
+++ cmd/krew/cmd/namingutils.go
@@ -0,0 +1,50 @@
+// Copyright 2019 The Kubernetes Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cmd
+
+import (
+	"sigs.k8s.io/krew/pkg/constants"
+	"sigs.k8s.io/krew/pkg/index"
+)
+
+// indexOf returns the index name of a receipt.
+func indexOf(r index.Receipt) string {
+	if r.Status.Source.Name == "" {
+		return constants.DefaultIndexName
+	}
+	return r.Status.Source.Name
+}
+
+// displayName returns the display name of a Plugin.
+// The index name is omitted if it is the default index.
+func displayName(p index.Plugin, indexName string) string {
+	if isDefaultIndex(indexName) {
+		return p.Name
+	}
+	return indexName + "/" + p.Name
+}
+
+func isDefaultIndex(name string) bool {
+	return name == "" || name == constants.DefaultIndexName
+}
+
+// canonicalName returns INDEX/NAME value for a plugin, even if
+// it is in the default index.
+func canonicalName(p index.Plugin, indexName string) string {
+	if isDefaultIndex(indexName) {
+		indexName = constants.DefaultIndexName
+	}
+	return indexName + "/" + p.Name
+}
