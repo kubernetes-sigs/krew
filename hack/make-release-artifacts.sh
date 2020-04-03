8b2817c76a2e168e877d1986e085eae7a76f704a
ericzhang08 <yuzhangfish@163.com>
2020-04-03 13:45:49 +0800

    build untagged commits with + to indicate commit

--- hack/make-release-artifacts.sh
+++ hack/make-release-artifacts.sh
@@ -71,7 +71,7 @@ git_describe="$(git describe --tags --dirty --always)"
 if [[ ! "${git_describe}" =~ v.* ]]; then
   # if tag cannot be inferred (e.g. CI/CD), still provide a valid
   # version field for krew.yaml
-  git_describe="v0.0.0-detached.${git_describe}"
+  git_describe="v0.0.0-detached+${git_describe}"
 fi
 krew_version="${TAG_NAME:-$git_describe}"
 cp ./hack/krew.yaml ./out/krew.yaml
