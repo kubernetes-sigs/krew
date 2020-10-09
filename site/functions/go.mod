3b31554e95db6e4a7163587615827d15fa541153
Ahmet Alp Balkan <ahmetb@google.com>
2020-10-09 13:08:33 -0700

    deneme
    Signed-off-by: Ahmet Alp Balkan <ahmetb@google.com>

---
+++ site/functions/go.mod
@@ -0,0 +1,5 @@
+module sigs.k8s.io/krew/site/functions
+
+go 1.15
+
+require github.com/aws/aws-lambda-go v1.19.1
---
+++ site/functions/hello/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+    "net/http"
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+func handler(request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
+	return &events.APIGatewayProxyResponse{
+		StatusCode:        200,
+		Headers:           map[string]string{"Content-Type": "text/plain"},
+		MultiValueHeaders: http.Header{"Set-Cookie": {"Ding", "Ping"}},
+		Body:              "Hello, World!",
+		IsBase64Encoded:   false,
+	}, nil
+}
+
+func main() {
+	// Make the handler available for Remote Procedure Call by AWS Lambda
+	lambda.Start(handler)
+}
