458aec01c4d1590df9f12a96e6dcb2890d27e167
Chris Kim <chriskim06@gmail.com>
2020-11-24 13:40:17 -0800

    Add simple unit test for DefaultIndex function

---
+++ pkg/index/util_test.go
@@ -0,0 +1,36 @@
+// Copyright 2020 The Kubernetes Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package index
+
+import (
+	"os"
+	"testing"
+
+	"sigs.k8s.io/krew/pkg/constants"
+)
+
+func TestDefaultIndex(t *testing.T) {
+	if got := DefaultIndex(); got != constants.DefaultIndexURI {
+		t.Errorf("expected DefaultIndex() to be %q; got=%q", constants.DefaultIndexURI, got)
+	}
+
+	want := "foo"
+	os.Setenv("KREW_DEFAULT_INDEX_URI", want)
+	defer os.Unsetenv("KREW_DEFAULT_INDEX_URI")
+
+	if got := DefaultIndex(); got != want {
+		t.Errorf("expected DefaultIndex() to be %q; got=%q", want, got)
+	}
+}
