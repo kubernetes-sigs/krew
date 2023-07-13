36d4b1ccc77de0cc105f0a3011c25190deef95de
Ahmet Alp Balkan <ahmet@linkedin.com>
2023-07-13 13:45:38 -0700

    bump golangci-lint
    seeing error
    
    ```
    Installing golangci-lint
    golangci/golangci-lint info checking GitHub for tag 'v1.44.0'
    golangci/golangci-lint info found version: 1.44.0 for v1.44.0/linux/amd64
    golangci/golangci-lint info installed /home/runner/go/bin/golangci-lint
    panic: load embedded ruleguard rules: rules/rules.go:13: can't load fmt
    
    goroutine 1 [running]:
    github.com/go-critic/go-critic/checkers.init.22()
    	github.com/go-critic/go-critic@v0.6.2/checkers/embedded_rules.go:46 +0x4b4
    ```

--- hack/run-lint.sh
+++ hack/run-lint.sh
@@ -23,7 +23,7 @@ gopath="$(go env GOPATH)"
 if ! [[ -x "$gopath/bin/golangci-lint" ]]; then
   echo >&2 'Installing golangci-lint'
   curl --silent --fail --location \
-    https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b "$gopath/bin" v1.44.0
+    https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b "$gopath/bin" v1.53.3
 fi
 
 # configured by .golangci.yml
