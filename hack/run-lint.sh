b86f5cbd524cf161c1712fe0bf84cfaba170c9a8
Marvin Beckers <mail@embik.me>
2024-04-14 11:04:36 +0200

    Update to golangci-lint v1.57.2
    Signed-off-by: Marvin Beckers <mail@embik.me>

--- hack/run-lint.sh
+++ hack/run-lint.sh
@@ -23,7 +23,7 @@ gopath="$(go env GOPATH)"
 if ! [[ -x "$gopath/bin/golangci-lint" ]]; then
   echo >&2 'Installing golangci-lint'
   curl --silent --fail --location \
-    https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b "$gopath/bin" v1.53.3
+    https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b "$gopath/bin" v1.57.2
 fi
 
 # configured by .golangci.yml
